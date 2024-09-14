1146121d6409652e163373f69ea9911743873b3d
unknown <denilson582000@gmail.com>
2024-09-14 12:42:46 +0200

    Ejercicios del 1 al 5 del capitulo 1 realizados

--- capitulo_1/Ej_1_3.rkt
+++ capitulo_1/Ej_1_3.rkt
@@ -1,10 +1,34 @@
 #lang racket
 
 
+#|
+**Exercise 1.3:** Define a procedure that takes three numbers as
+arguments and returns the sum of the squares of the two larger
+numbers.
+|#
+
 (define (square a b c)
-  (cond ((and (> a b) (> c b)) (+(* a a) (* c c)))
-        ((and (> b a) (> c a)) (+(* b b) (* c c)))
-        ((and (> a c) (> b c)) (+(* a a) (* b b))))
+  (cond ((or(and (> a b) (> c b))  (and (= a b) (> c a)) (= a b c))  (+(* a a) (* c c)))
+        ((or(and (> b a) (> c a)) (and (= a c) (> b a)) (= a b c)) (+(* b b) (* c c)))
+        ((or(and (> a c) (> b c)) (and (= b c) (> a b)) (= a b c)) (+(* a a) (* b b))))
   )
  
-         
+#|
+Esta función llamada square que se requiere en el ejercicio toma como
+argumento 3 números y para que se cumpla con la función pedida (devolver
+la suma de los cuadrados de los dos números más grandes); se realizan 3
+comparaciones:
+ -En la primera se comprueba si a es mayor que b y c es mayor que b; o si a es igual que
+ b y c es mayor que a; o si los tres valores son iguales.
+ -En la segunda se verifica si b es mayor que a y c es mayor que a; o si a es igual que c
+ y b es mayor que a; o si los tres valores son iguales.
+ -En la tercera se comprueba si a es mayor que c y b es mayor que c; o si b es igual que c
+ y a es mayor que b; o si los tres valores son iguales.
+
+
+Entonces, al cumplirse alguna de las condiciones, se realizará la operación requerida, pero hay
+que mencionar que si los tres valores son iguales se hace la suma de los cuadrados de 2 de ellos,
+y para el caso de las condiciones de que hayan 2 iguales; he tenido en cuenta las poibles operaciones
+resultantes.
+
+|#   
--- capitulo_1/Ej_1_5.rkt
+++ capitulo_1/Ej_1_5.rkt
@@ -10,18 +10,20 @@
 (test 0 (p))
 
 #|
+
+Explicación:
+
 Lo que ocurrirá si esta función usa el orden aplicativo es que
-antes de ejecutarse la función se intenta obtener el valor de los argumentos
+antes de ejecutarse la función se intentará obtener el valor de los argumentos
 y con este ejemplo al evaluar el segundo argumento ((p)) hace que se quede como en
 un bucle infinito y no se termina el programa, ni da un valor y no se llega a comparar
-el valor de x con 0. Probé usar como argumento p sin los paréntesis y en este caso si me devolvía 0
-y no se quedaba en bucle. En conclusión, al llamar a una función y al pasarle un argumento dentro de un paréntesis
-, hace que se quede en bucle.
+el valor de x con 0. Probé a usar como argumento p sin los paréntesis y en este caso si me devolvía 0
+y no se quedaba en bucle.
 
 Ej:
 (test 0 p)
 
-Por otro lado, si se usa el orden normal, lo que pasaría es que como no intenta obtener los valores
+Por otro lado, si se usa el orden normal, lo que pasaría es que como no se intenta obtener los valores
 de los argumentos antes de ejecutar esta, sino dentro de la misma si su uso es requerido, es que la
 función retornaría un 0 y es lo que se mostraría porque se cumple la condición dentro de la función
 y esto evita que se requiera obtener el valor del segundo argumento.
