6734b6969a55326b563cb97c7bf07a6ef4a9c772
Iñigo <iaramburu2000@gmail.com>
2024-09-27 16:43:23 +0200

    Ejercicios semana del 16, otra vez

---
+++ capitulo_1/ej_1_3.rkt
@@ -0,0 +1,5 @@
+#lang racket
+(define (funcion a b c) (
+                         cond ((and (>= a c) (>= b c)) (+ (* a a) (* b b)))
+                              ((and (>= a b) (>= c b)) (+ (* a a) (* c c)))
+                              ((and (>= b a) (>= c a)) (+ (* c c) (* b b)))))
